e18c1e2658bdf4d1b1c503a0b92d004994c46284
ahmedmazigh <am.ahmed.mazigh@gmail.com>
2024-10-10 22:26:49 +0100

    adding _str_()

--- categories/models.py
+++ categories/models.py
@@ -14,5 +14,8 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name_plural = "Categories"
