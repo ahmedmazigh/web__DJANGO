bd7878ed7582f0e72351408d163b9b2be9d36def
ahmedmazigh <am.ahmed.mazigh@gmail.com>
2024-10-10 21:55:05 +0100

    conferences interface admin

--- conferences/admin.py
+++ conferences/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 from .models import Conference
 admin.site.register(Conference)
+
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'location', 'category', 'capacity', 'price')
+    search_fields = ['title']
+    ordering = ['start_date']
+    list_filter = ['category', 'start_date']
+    readonly_fields = ('created_at', 'update_at')
+    
+    fieldsets = (
+        ('Informations générales', {
+            'fields': ('title', 'description', 'category', 'location', 'capacity', 'price')
+        }),
+        ('Horaires', {
+            'fields': ('start_date', 'end_date')
+        }),
+        ('Documents', {
+            'fields': ('program',)
+        }),
+        ('Dates de suivi', {
+            'fields': ('created_at', 'update_at'),
+            'classes': ('collapse',)  # Masque ces champs par défaut
+        }),
+    )
