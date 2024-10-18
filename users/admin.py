2dca19448d2dfb7180c35b7e47b352b9c325f3c6
ahmedmazigh <am.ahmed.mazigh@gmail.com>
2024-10-18 10:01:20 +0100

    fixed filter

--- users/admin.py
+++ users/admin.py
@@ -16,4 +16,18 @@ class ParticipantAdmin(admin.ModelAdmin):
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('conference', 'participant', 'confirmed', 'reservation_date')
     search_fields = ['conference__title', 'participant__username']
-    list_filter = ['confirmed']  
+    list_filter = ['confirmed']
+    
+    def make_confirmed(self, request, queryset):
+        queryset.update(confirmed=True)
+        self.message_user(request, "Selected reservations have been confirmed.")
+
+    make_confirmed.short_description = "Mark selected reservations as confirmed"
+    
+    def make_not_confirmed(self, request, queryset):
+        queryset.update(confirmed=False)
+        self.message_user(request, "Selected reservations have been unconfirmed.")
+
+    make_not_confirmed.short_description = "Mark selected reservations as not confirmed"
+    
+    actions = [make_confirmed, make_not_confirmed]
